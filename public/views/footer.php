b984ec4f01d15dbabc2e3c120efd695e01575361
hetop <hetop@ya.ru>
2019-08-04 01:23:59 +0300

    Add service worker & offline page

--- public/views/footer.php
+++ public/views/footer.php
@@ -1,3 +1,8 @@
 <div class="created">© — Kantain Studio, 2019 <a href="https://vk.me/hiddder" target="_blank"><img class="social-link" src="/assets/icon/vk-social-network-logo.svg" alt="VK logo"></a></div></div>
+<script>
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('./serviceworker.js');
+	}
+</script>
 </body>
 </html>
