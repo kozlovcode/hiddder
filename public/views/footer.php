80de422ec20e2c291e52bb08dc9487b4e94d42bd
kozlovcode <romandkoz@gmail.com>
2019-08-05 18:50:57 +0300

    Add github link

--- public/views/footer.php
+++ public/views/footer.php
@@ -1,3 +1,3 @@
-<div class="created">© — Kantain Studio, 2019 <a href="https://vk.me/hiddder" target="_blank"><img class="social-link" src="/assets/icon/vk-social-network-logo.svg" alt="VK logo"></a><a href="https://t.me/hiddder_bot" target="_blank"><img class="social-link" src="/assets/icon/telegram.svg" alt="Telegram logo"></a></div></div>
+<div class="created">© — Kantain Studio, 2019 <a href="https://vk.me/hiddder" target="_blank"><img class="social-link" src="/assets/icon/vk-social-network-logo.svg" alt="VK logo"></a><a href="https://t.me/hiddder_bot" target="_blank"><img class="social-link" src="/assets/icon/telegram.svg" alt="Telegram logo"></a><a href="https://github.com/kozlovcode/hiddder" target="_blank"><img class="social-link" src="/assets/icon/github.svg" alt="Github logo"></a></div></div>
 </body>
 </html>
