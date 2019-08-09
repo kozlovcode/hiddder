a6ecb1fa15a3c3c7634d696e68dddb20133bb4e1
Kozlovcode <romandkoz@gmail.com>
2019-08-10 01:17:34 +0300

    Create views counter

--- public/modules/redirect.php
+++ public/modules/redirect.php
@@ -3,6 +3,9 @@ $redirect_url = explode('/', $_SERVER['REQUEST_URI']);
 $links = R::findOne('link', 'hash = ?', [$redirect_url[1]]);
 $link = $links->url;
 if ($links->hash == $redirect_url[1]) {
+	$views = R::load('link', $links->id);
+	$views->views = ($views->views + 1);
+	R::store($views);
 	header('Location: '.rawurldecode($link),true,301);
 }else{
 	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
